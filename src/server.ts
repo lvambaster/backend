4cfc55dc75da236b80bd9b690f117bdb5da505a1
lvambaster <lvambaster@gmail.com>
2025-09-12 08:37:25 -0300

    server delete fileupload

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,12 @@ import 'express-async-errors';
 import cors from 'cors';
 
 import { router } from './routes'
-import fileUpload from 'express-fileupload';
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 }
-}));
+
 
 app.use(router);
 
