ce0ba86b4d5bd76350bcd2dcb946af075bc2633d
lvambaster <lvambaster@gmail.com>
2025-09-12 08:54:20 -0300

    server.ts
    teste sem app.listem

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,25 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-
-import { router } from './routes'
-
+import { router } from './routes';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
-
 app.use(router);
 
+// Middleware de erro
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if(err instanceof Error){
-    //Se for uma instancia do tipo error
-    return res.status(400).json({
-      error: err.message
-    })
+  if (err instanceof Error) {
+    return res.status(400).json({ error: err.message });
   }
 
   return res.status(500).json({
     status: 'error',
-    message: 'Internal server error.'
-  })
+    message: 'Internal server error.',
+  });
+});
 
-})
-
-app.listen(process.env.PORT, () => console.log('Servidor online!!!!'))
+// 🚨 Importante: não usar app.listen no Vercel
+export default app;
